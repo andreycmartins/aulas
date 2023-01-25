fa24f74779cc6e6d8a2864a0447625f854319202
Andrey Martins <Andreybr45@gmail.com>
2023-01-24 21:56:43 -0300

    feito uso de history e link

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,17 @@
+import { Link, useHistory } from "react-router-dom"
+
 export const Dashboard = () => {
-  return <p>Dashboard</ p>
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/pagina-inicial")
+  }
+
+  return (
+    <div>
+      <p>Dashboard</p>
+      <Link to="/login">login</Link>
+    </div>
+  )
 }
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom"
-import { Dashboard } from "../pages"
+import { Dashboard, Login } from "../pages"
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="*" element={<Dashboard />} />
       </Switch>
