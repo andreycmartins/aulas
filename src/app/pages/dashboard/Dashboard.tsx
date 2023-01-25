20ac00a80b65a573254e8b879b3a2e37f74caa1b
Andrey Martins <Andreybr45@gmail.com>
2023-01-24 22:38:01 -0300

    removido history

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,9 @@
-import { Link, useHistory } from "react-router-dom"
 
 export const Dashboard = () => {
-
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.push("/pagina-inicial")
-  }
 
   return (
     <div>
       <p>Dashboard</p>
-      <Link to="/login">login</Link>
     </div>
   )
 }
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,7 +1,16 @@
 export const Login = () => {
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">
+          <span>Email</span>
+          <input/>
+        </label>
+        <label htmlFor="">
+          <span>Senha</span>
+          <input type="password"/>
+        </label>
+      </form>
+    </div>
+  )
 }
