062b5dbe0ee2231161cb2670a63a308b9b860f24
Andrey Martins <Andreybr45@gmail.com>
2023-01-24 22:57:50 -0300

    implementação de useState

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react"
+
 export const Login = () => {
+
+  const [email, setEmail] = useState('teste')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {console.log(email, password)}
+
   return (
     <div>
-      <form action="">
-        <label htmlFor="">
+      <form>
+        <label>
           <span>Email</span>
-          <input/>
+          <input value={email} onChange={e => setEmail(e.target.value)} />
         </label>
-        <label htmlFor="">
+        <label>
           <span>Senha</span>
-          <input type="password"/>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
+        <button type="button" onClick={handleLogin}>
+          Entrar
+        </button>
       </form>
     </div>
   )
